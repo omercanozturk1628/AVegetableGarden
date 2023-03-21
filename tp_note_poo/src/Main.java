7a74870fe15497b1479f156b9c77717953297be3
Franklin <franklin.assogba@etu.univ-lyon1.fr>
2023-03-21 17:17:07 +0100

    code de base importé

--- tp_note_poo/src/Main.java
+++ tp_note_poo/src/Main.java
@@ -1,5 +1,13 @@
+import VueControleur.VueControleurPotager;
+import modele.Ordonnanceur;
+import modele.SimulateurPotager;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimulateurPotager simulateurPotager = new SimulateurPotager();
+        VueControleurPotager vc = new VueControleurPotager(simulateurPotager);
+        vc.setVisible(true);
+        Ordonnanceur.getOrdonnanceur().addObserver(vc);
+        Ordonnanceur.getOrdonnanceur().start(300);
+
     }
 }
